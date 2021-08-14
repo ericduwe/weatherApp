15cf844b881514898a72ea6cb3ae6fa0b20b712c
Eric Duwe <ericduwe@gmail.com>
2021-08-14 13:06:41 -0500

    URL now fetches data for city specified in search form; still need to add functionality for state or country codes

--- assets/script.js
+++ assets/script.js
@@ -2,14 +2,13 @@ var weatherContainerEl = document.getElementById("weatherContainer");
 var searchForm = document.getElementById("search-form");
 var searchButton = document.getElementById("button-addon2");
 
-let apiKey = "aed951678fe40a952b0d63a1ad23589b";
-let queryUrl = `https://api.openweathermap.org/data/2.5/forecast?q=Dallas&appid=${apiKey}`
+
 
 
 function searchApi(query) {
     var apiKey = "aed951678fe40a952b0d63a1ad23589b";
-    var queryUrl = `https://api.openweathermap.org/data/2.5/forecast/daily?q=Dallas,TX&cnt=5&appid=${apiKey}`
-
+    var queryUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${query}&appid=${apiKey}`
+//api key is invalid -- need to fix
 
     fetch(queryUrl)
 
@@ -24,16 +23,16 @@ function searchApi(query) {
             weatherContainerEl.appendChild(cityName);
             console.log(data);
             
-            var 
-            if (!data.results.length) {
-                console.log("No results found");
-                weatherContainerEl.innerHTML = "<h3>No results found, search again!</h3>";
-            } else {
-                weatherContainerEl.textContent = "";
-                for (var i=0; i < data.results.length;i++) {
-                     printResults(weatherContainer.results[i]);
-                }
-            }
+            // var 
+            // if (!data.results.length) {
+            //     console.log("No results found");
+            //     weatherContainerEl.innerHTML = "<h3>No results found, search again!</h3>";
+            // } else {
+            //     weatherContainerEl.textContent = "";
+            //     for (var i=0; i < data.results.length;i++) {
+            //          printResults(weatherContainer.results[i]);
+            //     }
+            // }
 
         })
 }
